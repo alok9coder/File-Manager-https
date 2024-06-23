5d9ffd39c1ba84e18770edb365e083cbab57e73d
Alok Singh <alok.singh9711@gmail.com>
2024-06-23 08:48:45 +0530

    Added some password authentication code google Oauth not added yet. Site still not Functional!

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
 
 const app = express();
-const port = 7000;
+const port = 7777;
 const saltRounds = 15;
 
 // Default Home Directory for the File Manager.
@@ -58,10 +58,13 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+/*
+// HTTPS .key and .cert files
 const options = {
     key: fs.readFileSync("server.key"),
     cert: fs.readFileSync("server.cert"),
 };
+*/
 
 
 app.get("/", async (req, res) => {
@@ -89,6 +92,104 @@ app.get("/register", (req, res) => {
     res.render("register.ejs", content);
 });
 
+app.get("/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"],
+    })
+);
 
+app.get("/auth/google/home/pi",
+    passport.authenticate("google", {
+        successRedirect: "/home/pi",
+        failureRedirect: "/login",
+    })
+);
 
+app.post("/login",
+    passport.authenticate("local", {
+        successRedirect: "/home/pi",
+        failureRedirect: "/login",
+    })
+)
 
+app.post("/register-account", async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+
+    try {
+        const checkResult = await db.query("SELECT * FROM users WHERE email = $1;",
+            [email]
+        );
+
+        if (checkResult.rows.length > 0) {
+            req.redirect("/login");
+        } else {
+            bcrypt.hash(password, saltRounds, async (error, hash) => {
+                if (error) {
+                    console.log("Error hashing password: ", error);
+                } else {
+                    const result = await db.query(
+                        "INSERT INTO USERS (email, password) VALUES ($1, $2) RETURNING *;",
+                        [email, hash]
+                    );
+                    const user = result.rows[0];
+                    req.login(user, (error) => {
+                        console.log("Success");
+                        res.redirect("/home/pi");
+                    })
+                }
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+passport.use(
+    "local",
+    new Strategy(async function verify(username, password, cb) {
+        try {
+            const result = await db.query("SELECT * FROM users WHERE email = $1;",
+                [username]
+            );
+            if (result.rows.length > 0) {
+                const user = result.rows[0];
+                const storedHashedPassword = user.password;
+                bcrypt.compare(password, storedHashedPassword, (error, valid) => {
+                    if (error) {
+                        console.log("Error comparing passwords: ", error);
+                        return cb(error);
+                    } else {
+                        if (valid) {
+                            return cb(null, user);
+                        } else {
+                            return cb(null, false);
+                        }
+                    }
+                });
+            } else {
+                return cb("User not Found!");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    })
+);
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
+});
+
+app.listen(port, () => {
+    console.log(`HTTP Server Listening on port ${port}`);
+});
+
+/*
+https.createServer(options, app).listen(port, (req, res) => {
+    console.log(`HTTPS Server is Listening on port: ${port}`);
+});
+*/
